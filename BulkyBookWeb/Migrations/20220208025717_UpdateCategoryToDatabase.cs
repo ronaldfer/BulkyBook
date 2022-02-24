f43ca087a33cdf3f183026f0fde3cceb2f44ce0b
ronaldfer <rfernandez@softpropr.com>
2022-02-23 20:31:16 -0400

    Add project files.

---
+++ BulkyBookWeb/Migrations/20220208025717_UpdateCategoryToDatabase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBookWeb.Migrations
+{
+    public partial class UpdateCategoryToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Categories",
+                newName: "FullName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Categories",
+                newName: "Name");
+        }
+    }
+}
---
+++ BulkyBookWeb/Migrations/20220208025900_NameCategoryToDatabase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBookWeb.Migrations
+{
+    public partial class NameCategoryToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Categories",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Categories",
+                newName: "FullName");
+        }
+    }
+}
